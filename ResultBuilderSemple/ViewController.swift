d31ccc025d5b2149b42298a784d28a128e8152b7
siwonLee <111dltldnjs@gmail.com>
2024-09-13 01:54:58 +0900

    예시코드 작성

--- ResultBuilderSemple/ViewController.swift
+++ ResultBuilderSemple/ViewController.swift
@@ -7,13 +7,118 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+@resultBuilder
+struct UIViewBuilder {
+  static func buildEither(first component: [UIView]) -> [UIView] {
+    component
+  }
+  
+  static func buildEither(second component: [UIView]) -> [UIView] {
+    component
+  }
+  
+  static func buildOptional(_ component: [UIView]?) -> [UIView] {
+    return component ?? []
+  }
+  
+  static func buildArray(_ components: [[UIView]]) -> [UIView] {
+    components.flatMap { $0 }
+  }
+  
+  static func buildPartialBlock(first: [UIView]) -> [UIView] {
+    return first
+  }
+  
+  static func buildPartialBlock(first: UIView) -> [UIView] {
+    return [first]
+  }
+  
+  static func buildPartialBlock(accumulated: [UIView], next: [UIView]) -> [UIView] {
+    return accumulated + next
+  }
+  
+  static func buildPartialBlock(accumulated: [UIView], next: UIView) -> [UIView] {
+    return accumulated + [next]
+  }
+}
 
+extension UIStackView {
+  convenience init(@UIViewBuilder content: () -> [UIView]) {
+    self.init(arrangedSubviews: content())
+  }
+}
+
+
+class ViewController: UIViewController {
+  
+  //  private lazy var mainStackView: UIStackView = {
+  //    let stack = UIStackView(arrangedSubviews: [fristStackView, secondStackView, thirdStackView])
+  //    stack.distribution = .fillEqually
+  //    return stack
+  //  }()
+  
+  private let fristStackView: UIStackView = {
+    let stack = UIStackView(arrangedSubviews: [
+      UIImageView(image: .init(systemName: "swift")),
+      UIImageView(image: .init(systemName: "swift")),
+      UIImageView(image: .init(systemName: "swift"))
+    ])
+    stack.axis = .vertical
+    stack.distribution = .fillEqually
+    return stack
+  }()
+  
+  private let secondStackView: UIStackView = {
+    let stack = UIStackView(arrangedSubviews: [
+      UIImageView(image: .init(systemName: "swift")),
+      UIImageView(image: .init(systemName: "swift")),
+      UIImageView(image: .init(systemName: "swift"))
+    ])
+    stack.axis = .vertical
+    stack.distribution = .fillEqually
+    return stack
+  }()
+  
+  private let thirdStackView: UIStackView = {
+    let stack = UIStackView(arrangedSubviews: [
+      UIImageView(image: .init(systemName: "swift")),
+      UIImageView(image: .init(systemName: "swift")),
+      UIImageView(image: .init(systemName: "swift"))
+    ])
+    stack.axis = .vertical
+    stack.distribution = .fillEqually
+    return stack
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.backgroundColor = .systemGroupedBackground
+    
+    //    for stack in [fristStackView, secondStackView, thirdStackView] {
+    //      for _ in 0..<3 {
+    //        stack.addArrangedSubview(UIImageView(image: .init(systemName: "swift")))
+    //      }
+    //    }
+    
+    let mainStackView = UIStackView {
+      fristStackView
+      
+      for view in [secondStackView, thirdStackView] {
+        view
+      }
+    }
+    
+    mainStackView.distribution = .fillEqually
+    
+    view.addSubview(mainStackView)
+    mainStackView.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      mainStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      mainStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      mainStackView.widthAnchor.constraint(equalToConstant: 300),
+      mainStackView.heightAnchor.constraint(equalToConstant: 300)
+    ])
   }
-
-
 }
 
